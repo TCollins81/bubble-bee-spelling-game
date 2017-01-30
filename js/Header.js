fa6ae8df54c6dd291da936ac5a561b94485afd8c
Daniel <dray364@gmail.com>
2017-01-30 11:07:55 -0600

    added header, footer, home, post, and resume .js files

--- js/Header.js
+++ js/Header.js
@@ -5,12 +5,12 @@ export default React.createClass({
   render() {
     return (
       <header className="title">
-        <h1 className="title"> Know Daniel </h1>
+        <h1 className="title"> Bubble-Bee </h1>
         <nav className="links">
           <Link to="/" className="home">Home</Link>
-          <Link to="/post" className="post">Blog</Link>
-          <Link to="/projects" className="projects">Projects</Link>
-          <Link to="/resume" className="resume">Resume</Link>
+          // <Link to="/post" className="post">Blog</Link>
+          // <Link to="/projects" className="projects">Projects</Link>
+          // <Link to="/resume" className="resume">Resume</Link>
         </nav>
       </header>
     )
