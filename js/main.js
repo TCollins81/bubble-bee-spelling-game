f8ce6849e23ccada38867311f3e44cd48b35b445
Daniel <dray364@gmail.com>
2017-01-30 10:56:13 -0600

    added Header.js file / modified index.html for css link to show public/styles/main.css pathway

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 // Grabbing react from node_modules
 import React from 'react'
+import Header from './Header'
+
+export default React.createClass({
+  render() {
+    return(
+      <section>
+
+      </section>
+    )
+  }
+})
