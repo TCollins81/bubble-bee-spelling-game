16dddca454f8ccee432f421c842777d9c780d120
Daniel <dray364@gmail.com>
2017-01-31 12:34:23 -0600

    added a second form for the input and login specifically for the student / trying to differentiate the two

--- js/Home.js
+++ js/Home.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+// Created form for login for teachers as they are going to need to login seperately so they can go to the teacher portal page where they will be prompted to enter words and grade level for game
+
+// Created form for login for students as they are going to login with the i.d that the teacher provides them
 export default React.createClass({
   render() {
     return (
       <form>
         <input className="teacher_login" placeholder=" Teacher Sign In" type="text"/>
         <button className="submit_button" type="submit"> Login </button>
+      </form>
+
+      <form>
         <input className="student_login" placeholder=" Student Sign In" type="text"/>
         <button className="submit_button" type="submit"> Login </button>
       </form>
