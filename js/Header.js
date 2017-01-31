e74705358a8416f1fe8b2c727ce9c8fe33f43871
Daniel <dray364@gmail.com>
2017-01-31 15:02:04 -0600

    corrected path for the bubbe-bee logo

--- js/Header.js
+++ js/Header.js
@@ -6,7 +6,7 @@ export default React.createClass({
     return (
       <header className="title">
         <nav className="links">
-          <img src=".public/styles/Bubble-Bee.png" alt="" className="Bubble-Bee-Image"/>
+          <img src="./public/styles/Bubble-Bee.png" alt="" className="Bubble-Bee-Image"/>
           <Link to="/" className="home">Home</Link>
         </nav>
       </header>
