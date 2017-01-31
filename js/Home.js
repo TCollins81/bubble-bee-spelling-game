a0e7601eab2e38f5bb98043f84f36fc0f83a9a5a
Daniel <dray364@gmail.com>
2017-01-31 12:36:02 -0600

    added container for both forms to be wrapped inside of

--- js/Home.js
+++ js/Home.js
@@ -7,15 +7,17 @@ import { Link } from 'react-router'
 export default React.createClass({
   render() {
     return (
-      <form>
-        <input className="teacher_login" placeholder=" Teacher Sign In" type="text"/>
-        <button className="submit_button" type="submit"> Login </button>
-      </form>
+      <section>
+        <form>
+          <input className="teacher_login" placeholder=" Teacher Sign In" type="text"/>
+          <button className="submit_button" type="submit"> Login </button>
+        </form>
 
-      <form>
-        <input className="student_login" placeholder=" Student Sign In" type="text"/>
-        <button className="submit_button" type="submit"> Login </button>
-      </form>
+        <form>
+          <input className="student_login" placeholder=" Student Sign In" type="text"/>
+          <button className="submit_button" type="submit"> Login </button>
+        </form>
+      </section>
     )
   }
 })
