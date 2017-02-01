c1f7add270000d2ff3a0d429380dd2b617ba3b7d
Daniel <dray364@gmail.com>
2017-02-01 00:07:03 -0600

    aligned all login form content to be center of the page

--- js/Home.js
+++ js/Home.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router'
 export default React.createClass({
   render() {
     return (
+      
       <div id="login_wrapper">
         <form name="login_form"
               className="login_form"
