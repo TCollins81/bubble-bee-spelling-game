557c04480c5decce3b8337622622a5e225d097c9
Daniel <dray364@gmail.com>
2017-02-07 00:03:19 -0600

    changed floating bubbles color to #00B5F3

--- js/Game.js
+++ js/Game.js
@@ -4,18 +4,22 @@ import { Link } from 'react-router'
 export default React.createClass({
   render() {
     return (
-          <div id="background-wrap">
-            <div className="bubble xx1"></div>
-            <div className="bubble xx2"></div>
-            <div className="bubble xx3"></div>
-            <div className="bubble xx4"></div>
-            <div className="bubble xx5"></div>
-            <div className="bubble xx6"></div>
-            <div className="bubble xx7"></div>
-            <div className="bubble xx8"></div>
-            <div className="bubble xx9"></div>
-            <div className="bubble xx10"></div>
-          </div>
+      <section className="gamebody">
+
+            <div id="background-wrap">
+              <div className="bubble xx1"></div>
+              <div className="bubble xx2"></div>
+              <div className="bubble xx3"></div>
+              <div className="bubble xx4"></div>
+              <div className="bubble xx5"></div>
+              <div className="bubble xx6"></div>
+              <div className="bubble xx7"></div>
+              <div className="bubble xx8"></div>
+              <div className="bubble xx9"></div>
+              <div className="bubble xx10"></div>
+            </div>
+
+      </section>
     )
   }
 })
