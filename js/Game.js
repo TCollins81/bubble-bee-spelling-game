35f3b3d12186851b662224e33494727a6bebaf37
Daniel <dray364@gmail.com>
2017-02-12 14:47:16 -0600

    Added more bubbles to game file/ added letters inside all bubbles by using a <p> tag

--- js/Game.js
+++ js/Game.js
@@ -7,16 +7,22 @@ export default React.createClass({
       <section className="gamebody">
 
             <div id="background-wrap">
-              <div className="bubble xx1"></div>
-              <div className="bubble xx2"></div>
-              <div className="bubble xx3"></div>
-              <div className="bubble xx4"></div>
-              <div className="bubble xx5"></div>
-              <div className="bubble xx6"></div>
-              <div className="bubble xx7"></div>
-              <div className="bubble xx8"></div>
-              <div className="bubble xx9"></div>
-              <div className="bubble xx10"></div>
+              <div className="bubble xx1"><p className="letter">A</p></div>
+              <div className="bubble xx2"><p className="letter">E</p></div>
+              <div className="bubble xx3"><p className="letter">I</p></div>
+              <div className="bubble xx4"><p className="letter">O</p></div>
+              <div className="bubble xx5"><p className="letter">U</p></div>
+              <div className="bubble xx6"><p className="letter">R</p></div>
+              <div className="bubble xx7"><p className="letter">D</p></div>
+              <div className="bubble xx8"><p className="letter">N</p></div>
+              <div className="bubble xx9"><p className="letter">L</p></div>
+              <div className="bubble xx10"><p className="letter">C</p></div>
+              <div className="bubble xx11"><p className="letter">M</p></div>
+              <div className="bubble xx12"><p className="letter">K</p></div>
+              <div className="bubble xx13"><p className="letter">S</p></div>
+              <div className="bubble xx14"><p className="letter">J</p></div>
+
+
             </div>
 
             <div>
